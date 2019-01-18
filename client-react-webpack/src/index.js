4b57031501aa634ab58f5cf9ce6a6f3f12959610
Julia <julyettka@gmail.com>
2019-01-18 17:10:59 +0200

    add webpack config

--- client-react-webpack/src/index.js
+++ client-react-webpack/src/index.js
@@ -1,2 +1,11 @@
-console.log("hello world");
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './scss/style.scss';
 
+const App = () => (
+	<div className="first_div">
+	<h1>Hello world</h1>
+</div>
+)
+
+ReactDOM.render(<App/>, document.getElementById('app'));
